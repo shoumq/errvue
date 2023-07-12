54ef236280b96d0dfaae32c3242fe8305eec2b0f
Shoumq <lae345@mail.ru>
2023-07-12 12:05:05 +0300

    css

--- resources/js/Pages/Login1.vue
+++ resources/js/Pages/Login1.vue
@@ -1,4 +1,17 @@
 <template>
+    <div class="drop_menu">
+        <div class="cont">
+            <a href="/" class="item">Продукты</a>
+            <a href="/login1" class="item">Создание 1</a>
+            <a href="/login2" class="item">Создание 2</a>
+            <a href="/login3" class="item">Создание 3</a>
+            <a href="/login4" class="item">Создание 4</a>
+            <a href="/login5" class="item">Создание 5</a>
+            <a href="/login6" class="item">Создание 6</a>
+            <a href="/login7" class="item">Создание 7</a>
+        </div>
+    </div>
+
     <header>
         <div class="container">
             <div class="flex">
@@ -10,6 +23,13 @@
                 <a href="/login5" class="item">Создание 5</a>
                 <a href="/login6" class="item">Создание 6</a>
                 <a href="/login7" class="item">Создание 7</a>
+            </div>
+
+            <div class="burger" id="burder" @click="openMenu">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="#FFFFFF" class="bi bi-list" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd"
+                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                </svg>
             </div>
         </div>
     </header>
@@ -67,6 +87,10 @@ export default {
             for (let i = 0; i < 3; i++) {
                 delete this.inputs[i].value
             }
+        },
+
+        openMenu: function () {
+            document.querySelector('.drop_menu').style.transform = 'translateX(0)'
         }
     }
 }
